7b17209331dc398e94a039a306e04ac800f9d31e
egorblagocinnov <blagochinnov.e.d@gmail.com>
2024-08-19 13:46:13 +0300

    add maven-publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "2.0.0"
+    id("java")
+    id("maven-publish")
 }
 
 group = "org.blagochinnoved"
@@ -15,6 +17,11 @@ tasks.test {
 
 kotlin {
     jvmToolchain(18)
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
 }
 
 sourceSets.main {
